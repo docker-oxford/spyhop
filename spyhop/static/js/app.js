b2eb837c9e771f58ac5135d07069a154b4cd8949
Jonatan <jonatan@jonatanblue.se>
2015-09-19 16:57:46 +0100

    Dictionary names corrected

--- spyhop/static/js/app.js
+++ spyhop/static/js/app.js
@@ -3,24 +3,24 @@ $(function () {
     var TIMEOUT = 1000, SHIFT = 20;
 
     $('#containers').dataTable({
-	"ajax": "/data/containers.json",
+	"ajax": "/api/ps",
 	"columns": [
-	    { "title": "Container ID", "data": "container-id" },
-	    { "title": "Image", "data": "image" },
-	    { "title": "Command", "data": "command" },
-	    { "title": "Created", "data": "created" },
-	    { "title": "Status", "data": "status" },
-	    { "title": "Ports", "data": "ports" },
-	    { "title": "Names", "data": "names" }
+	    { "title": "Container ID", "data": "Id" },
+	    { "title": "Image", "data": "Image" },
+	    { "title": "Command", "data": "Command" },
+	    { "title": "Created", "data": "Created" },
+	    { "title": "Status", "data": "Status" },
+	    { "title": "Ports", "data": "Ports[0].PublicPort" },
+	    { "title": "Names", "data": "Names" }
 	]
     });
 
     function rx_bytes() {
 
-	$.getJSON('/data/stats.json', function (data) {
+	$.getJSON('/api/db72a8931dcd', function (data) {
 
 	    var series = chart.series[0], shift = series.data.length > SHIFT;
-	    chart.series[0].addPoint(data.network.rx_bytes, true, shift);
+	    chart.series[0].addPoint(data.cpu_stats.cpu_usage.total_usage, true, shift);
 	    setTimeout(rx_bytes, TIMEOUT);
 
 	});
