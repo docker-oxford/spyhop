1d0bc90ccd0520c84ee4ac559e5b0fc815b96e6f
Chris Seymour <christopher.seymour@nanoporetech.com>
2015-09-21 09:48:35 +0100

    fixed async bug with cpuPercent

--- spyhop/static/js/app.js
+++ spyhop/static/js/app.js
@@ -1,88 +1,96 @@
 $(function () {
 
-    var RELOAD = 25000, TIMEOUT = 5000, SHIFT = 20, PERCISION = 2;
+    var RELOAD = 5000, TIMEOUT = 2000, SHIFT = 20, PERCISION = 2;
 
     var SERIES = {};
 
     var table = $('#containers').DataTable({
 	"ajax": "/api/ps",
 	"columns": [
-	    { "title": "Container ID", "data": "Id" },
-	    { "title": "Image", "data": "Image" },
-	    { "title": "Command", "data": "Command" },
-	    { "title": "Created", "data": "Created" },
-	    { "title": "Status", "data": "Status" },
-	    { "title": "Ports", "data": "Ports[0].PublicPort" },
-	    { "title": "Names", "data": "Names" }
+            { "title": "Container ID", "data": "Id" },
+            { "title": "Image", "data": "Image" },
+            { "title": "Command", "data": "Command" },
+            { "title": "Created", "data": "Created" },
+            { "title": "Status", "data": "Status" },
+            { "title": "Ports", "data": "Ports[0].PublicPort" },
+            { "title": "Names", "data": "Names" }
 	],
+        "aoColumnDefs": [{
+            "aTargets": [ 0 ],
+            "mRender": function (data, type, full) {
+                return data.slice(0, 12);
+            }
+        }],
 	"fnInitComplete": function (settings) {
 
-	    if (settings.json) {
+            if (settings.json) {
+
 		for (var i = 0; i < settings.json.data.length; i++) {
 
-		    var hash = settings.json.data[i].Id;
+                    var hash = settings.json.data[i].Id.slice(0, 12);
 
-		    SERIES[hash] = chart.addSeries({
-			name: settings.json.data[i].Names,
-		    });
+                    SERIES[hash] = chart.addSeries({
+                        name: hash
+                    });
 
 		}
-		cpuPercent();
-	    }
+		updateCharts();
+		setInterval(updateCharts, TIMEOUT);
+            }
 	},
 
 	"fnDrawCallback": function(settings) {
 
-	    if (settings.json) {
+            if (settings.json) {
 
 		for (var i = 0; i < settings.json.data.length; i++) {
-		    console.log(settings.json.data[i].Id);
+                    console.log(settings.json.data[i].Id);
 		}
-	    }
+            }
 	}
+
     });
 
-
-    setInterval(function () {
+    setTimeout(function () {
 	table.ajax.reload(null, false);
     }, RELOAD);
 
 
-    function cpuPercent() {
+    function getStats(hash) {
 
-	for (var containerId in SERIES) {
+        $.getJSON('/api/curated/' + hash, function (data) {
 
-	    $.getJSON('/api/curated/' + containerId, function (data) {
+            var series = SERIES[hash], shift = series.data.length > SHIFT;
+            series.addPoint(parseFloat(data.cpu_percent.toFixed(PERCISION)), true, shift);
 
-		console.log('calling /api/curated/' + containerId);
+        });
 
-		var series = SERIES[containerId], shift = series.data.length > SHIFT;
-		series.addPoint(parseFloat(data.cpu_percent.toFixed(PERCISION)), true, shift);
-		setTimeout(cpuPercent, TIMEOUT);
+    }
 
-	    });
+    function updateCharts() {
+
+	for (var hash in SERIES) {
+            getStats(hash);
 	}
     }
 
     var chart = new Highcharts.Chart({
 
 	chart: {
-	    renderTo: 'charts',
+            renderTo: 'charts'
 	},
-
 	credits: {
-	    enabled: false,
+            enabled: false
 	},
 
 	title: {
-	    text : 'CPU %'
+            text : 'CPU %'
 	},
 
 	exporting: {
-	    enabled: false
-	},
+            enabled: false
+	}
 
     });
-
 
 });
