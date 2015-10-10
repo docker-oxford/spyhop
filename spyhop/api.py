36fa49aad1b7be8a2a4041624247644d93b97048
Jonatan <jonatan@jonatanblue.se>
2015-10-10 14:12:21 +0100

    Created Api class to hold Docker socket, configurable from command line

--- spyhop/api.py
+++ spyhop/api.py
@@ -3,70 +3,74 @@ from docker import Client
 from flask import jsonify
 import json
 
+class Api:
+    def __init__(self, socket='unix://var/run/docker.sock'):
+        docker_socket = socket
+        self.connection = Client(base_url=docker_socket)
 
-def calculate_cpu_percent(stats1, stats2):
-    cpu1 = stats1['cpu_stats']['cpu_usage']['total_usage']
-    system1 = stats1['cpu_stats']['system_cpu_usage']
-    cpu2 = stats2['cpu_stats']['cpu_usage']['total_usage']
-    system2 = stats2['cpu_stats']['system_cpu_usage']
-    # Calculate deltas
-    cpu_delta = float(cpu2 - cpu1)
-    system_delta = float(system2 - system1)
-    # Calculate percent
-    if (system_delta > 0.0 and cpu_delta > 0.0):
-        cpu_percent = (cpu_delta / system_delta) * len(stats2['cpu_stats']['cpu_usage']['percpu_usage']) * 100.0
-    else:
-        cpu_percent = 0.0
+    def calculate_cpu_percent(self, stats1, stats2):
+        cpu1 = stats1['cpu_stats']['cpu_usage']['total_usage']
+        system1 = stats1['cpu_stats']['system_cpu_usage']
+        cpu2 = stats2['cpu_stats']['cpu_usage']['total_usage']
+        system2 = stats2['cpu_stats']['system_cpu_usage']
+        # Calculate deltas
+        cpu_delta = float(cpu2 - cpu1)
+        system_delta = float(system2 - system1)
+        # Calculate percent
+        if (system_delta > 0.0 and cpu_delta > 0.0):
+            cpu_percent = (cpu_delta / system_delta) * len(stats2['cpu_stats']['cpu_usage']['percpu_usage']) * 100.0
+        else:
+            cpu_percent = 0.0
 
-    if cpu_percent < 150:
-        return cpu_percent
-    else:
-        return 0.0
+        if cpu_percent < 150:
+            return cpu_percent
+        else:
+            return 0.0
 
-def calculate_memory_percent(stats):
-    memory_percent = (float(stats['memory_stats']['usage']) / float(stats['memory_stats']['limit'])) * 100
-    print(memory_percent)
-    return memory_percent
+    def calculate_memory_percent(self, stats):
+        memory_percent = (float(stats['memory_stats']['usage']) / float(stats['memory_stats']['limit'])) * 100
+        print(memory_percent)
+        return memory_percent
 
-def calculate_network(stats1, stats2):
-    rx1 = stats1['network']['rx_bytes']
-    rx2 = stats2['network']['rx_bytes']
-    tx1 = stats1['network']['tx_bytes']
-    tx2 = stats2['network']['tx_bytes']
-    data_in = rx2 - rx1
-    data_out = tx2 - tx1
-    return data_in, data_out
+    def calculate_network(self, stats1, stats2):
+        rx1 = stats1['network']['rx_bytes']
+        rx2 = stats2['network']['rx_bytes']
+        tx1 = stats1['network']['tx_bytes']
+        tx2 = stats2['network']['tx_bytes']
+        data_in = rx2 - rx1
+        data_out = tx2 - tx1
+        return data_in, data_out
 
-def get_container_stats(container_id):
-    cli = Client(base_url='unix://var/run/docker.sock')
-    try:
-        stats = cli.stats(container_id)
-        return stats
-    except Exception, error:
-        return error
+    def get_container_stats(self, container_id):
+        cli = self.connection
+        try:
+            stats = cli.stats(container_id)
+            return stats
+        except Exception, error:
+            return error
 
-def get_curated_stats(container_id):
-    # Get generator and two stats packets
-    stats_generator = get_container_stats(container_id)
-    stats1 = json.loads(stats_generator.next())
-    stats2 = json.loads(stats_generator.next())
-    # Calculate curated stats
-    cpu_percent = calculate_cpu_percent(stats1, stats2)
-    mem_percent = calculate_memory_percent(stats2)
-    bytes_in, bytes_out = calculate_network(stats1, stats2)
-    # Put stats into JSON
-    monitoring_stats = {
-        "cpu_percent": cpu_percent,
-        "mem_percent": mem_percent,
-        "bytes_in": bytes_in,
-        "bytes_out": bytes_out
-    }
-    return monitoring_stats
+    def get_curated_stats(self, container_id):
+        # Get generator and two stats packets
+        stats_generator = self.get_container_stats(container_id)
+        stats1 = json.loads(stats_generator.next())
+        stats2 = json.loads(stats_generator.next())
+        # Calculate curated stats
+        cpu_percent = self.calculate_cpu_percent(stats1, stats2)
+        mem_percent = self.calculate_memory_percent(stats2)
+        bytes_in, bytes_out = self.calculate_network(stats1, stats2)
+        # Put stats into JSON
+        monitoring_stats = {
+            "cpu_percent": cpu_percent,
+            "mem_percent": mem_percent,
+            "bytes_in": bytes_in,
+            "bytes_out": bytes_out
+        }
+        return monitoring_stats
 
-def get_all_containers():
-    cli = Client(base_url='unix://var/run/docker.sock')
-    containers = cli.containers()
-    return containers
+    def get_all_containers(self):
+        cli = self.connection
+        containers = cli.containers()
+        return containers
 
 
 def main():
--- spyhop/spyhop.py
+++ spyhop/spyhop.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 from flask import Flask, jsonify
-from api import get_container_stats, get_all_containers, \
-    get_curated_stats
+import argparse
+from api import Api
 import json
 
 app = Flask(__name__)
+
+# Get command line arguments
+parser = argparse.ArgumentParser(description='Run Spyhop')
+parser.add_argument('--socket', metavar='SOCKET', type=str, nargs='?', default='unix://var/run/docker.sock',
+                   help='Socket on which to talk to Docker Daemon')
+args = parser.parse_args()
+socket = args.socket
+
+# Create API connection
+docker_api = Api(socket)
 
 @app.route('/')
 def index():
@@ -12,17 +22,17 @@ def index():
 
 @app.route('/api/<container_id>')
 def stats(container_id):
-    container_stats = get_container_stats(container_id)
+    container_stats = docker_api.get_container_stats(container_id)
     return jsonify(json.loads(container_stats.next()))
 
 @app.route('/api/curated/<container_id>')
 def curated_stats(container_id):
-    stats = get_curated_stats(container_id)
+    stats = docker_api.get_curated_stats(container_id)
     return jsonify(stats)
 
 @app.route('/api/ps')
 def ps():
-    all_containers = get_all_containers()
+    all_containers = docker_api.get_all_containers()
     return jsonify({"data": all_containers})
 
 if __name__ == '__main__':
